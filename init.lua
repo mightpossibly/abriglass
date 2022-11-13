19e2acca2f636fad257b08f6990a12271610ebb5
mightpossibly <111494670+mightpossibly@users.noreply.github.com>
2022-11-13 02:47:10 +0100

    Update init.lua

--- init.lua
+++ init.lua
@@ -6,3 +6,9 @@ dofile(modpath.."nodes.lua")
 dofile(modpath.."crafting.lua")
 
 abriglass.init = true
+
+minetest.register_alias("abriglass:stained_glass_light_gray", "abriglass:stained_glass_grey")
+minetest.register_alias("abriglass:stained_glass_dark_gray", "abriglass:stained_glass_dark_grey")
+minetest.register_alias("abriglass:stained_glass_purple", "abriglass:stained_glass_violet")
+minetest.register_alias("abriglass:stained_glass_frosted", "abriglass:stained_glass_white")
+minetest.register_alias("abriglass:stained_glass_dark_yellow", "abriglass:stained_glass_brown")
